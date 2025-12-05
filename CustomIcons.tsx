aa099f3403dab57de5fd0d94a470204669ecfbd3
Andrew Perry <andrew@cashcam.app>
2025-12-05 21:14:08 +1100

    Resize ThreeQuestionMarks icon to fit better

--- CustomIcons.tsx
+++ CustomIcons.tsx
@@ -11,22 +11,24 @@ export const ThreeQuestionMarks = ({ className, ...props }: React.SVGProps<SVGSV
         className={className}
         {...props}
     >
-        {/* Center Question Mark - Slightly larger/higher */}
-        <g transform="translate(0, -1)">
-            <path d="M12 16v-1.5c0-1.1.9-1.8 1.5-2.3.8-.6 1.5-1.4 1.5-2.7 0-1.7-1.3-3-3-3-1.4 0-2.6.9-2.9 2.2" />
-            <path d="M12 19h.01" strokeWidth="3" />
-        </g>
+        <g transform="translate(12, 12) scale(0.75) translate(-12, -12)">
+            {/* Center Question Mark - Slightly larger/higher */}
+            <g transform="translate(0, -1)">
+                <path d="M12 16v-1.5c0-1.1.9-1.8 1.5-2.3.8-.6 1.5-1.4 1.5-2.7 0-1.7-1.3-3-3-3-1.4 0-2.6.9-2.9 2.2" />
+                <path d="M12 19h.01" strokeWidth="3" />
+            </g>
 
-        {/* Left Question Mark - Tilted Left, Smaller */}
-        <g transform="translate(-6, 2) rotate(-15, 6, 12) scale(0.85)">
-            <path d="M6 16v-1.5c0-1.1.9-1.8 1.5-2.3.8-.6 1.5-1.4 1.5-2.7 0-1.7-1.3-3-3-3-1.4 0-2.6.9-2.9 2.2" />
-            <path d="M6 19h.01" strokeWidth="3" />
-        </g>
+            {/* Left Question Mark - Tilted Left, Smaller */}
+            <g transform="translate(-6, 2) rotate(-15, 6, 12) scale(0.85)">
+                <path d="M6 16v-1.5c0-1.1.9-1.8 1.5-2.3.8-.6 1.5-1.4 1.5-2.7 0-1.7-1.3-3-3-3-1.4 0-2.6.9-2.9 2.2" />
+                <path d="M6 19h.01" strokeWidth="3" />
+            </g>
 
-        {/* Right Question Mark - Tilted Right, Smaller */}
-        <g transform="translate(6, 2) rotate(15, 18, 12) scale(0.85)">
-            <path d="M18 16v-1.5c0-1.1.9-1.8 1.5-2.3.8-.6 1.5-1.4 1.5-2.7 0-1.7-1.3-3-3-3-1.4 0-2.6.9-2.9 2.2" />
-            <path d="M18 19h.01" strokeWidth="3" />
+            {/* Right Question Mark - Tilted Right, Smaller */}
+            <g transform="translate(6, 2) rotate(15, 18, 12) scale(0.85)">
+                <path d="M18 16v-1.5c0-1.1.9-1.8 1.5-2.3.8-.6 1.5-1.4 1.5-2.7 0-1.7-1.3-3-3-3-1.4 0-2.6.9-2.9 2.2" />
+                <path d="M18 19h.01" strokeWidth="3" />
+            </g>
         </g>
     </svg>
 );
